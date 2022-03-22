a16d6437562dd9725abe9158b889bdc9790821d4
Kalexpire <alex.sousa19@aluno.ce.gov.br>
2022-03-22 12:28:49 -0300

    Navbar + Product Card + Catalog + Routes + react-router-dom install

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+const ButtonIcon =() => {
+    return (
+     <button className='brn btn-primary btn-icon'>
+        <h6>INICIE AGORA SUA BUSCA</h6>
+     </button>
+  
+      );
+  
+  }
+  
+  export default ButtonIcon;
+  
